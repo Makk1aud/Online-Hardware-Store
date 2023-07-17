8f8a2b0f544060824185358ebca3a735e3be0ad0
Makk1aud <1roman080283@mail.ru>
2023-07-17 14:22:08 +0600

    Made new page and class

---
+++ DataApp/DBInteractClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Online_hardware_store.DataApp
+{
+    public static class DBInteractClass
+    {
+        public static string conString = "SERVER=LOCALHOST;DATABASE=online hardware store;UID=makklaud;PASSWORD=UAZ9233;";
+        private static MySqlConnection con = new MySqlConnection(conString);
+
+    }
+}
--- Pages/GeneralPages/PageLeftPanel.xaml.cs
+++ Pages/GeneralPages/PageLeftPanel.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Online_hardware_store.Pages.GeneralPages;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,19 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MySql.Data.MySqlClient;
 
-namespace Online_hardware_store
+namespace Online_hardware_store.Pages.GeneralPages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для PageLeftPanel.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PageLeftPanel : Page
     {
-        public MainWindow()
+        public PageLeftPanel()
         {
             InitializeComponent();
-            frameTopPanel.Navigate(new PageTopPanel());
         }
     }
 }
