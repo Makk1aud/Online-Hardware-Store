b03f30a03549040461350402f7fe26158cd66f21
Makk1aud <1roman080283@mail.ru>
2023-07-20 16:47:11 +0600

    make Page

---
+++ Pages/SubcategoriesWindowPages/SubcategoriesPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Online_hardware_store.Pages.SubcategoriesWindowPages
+{
+    /// <summary>
+    /// Логика взаимодействия для SubcategoriesPage.xaml
+    /// </summary>
+    public partial class SubcategoriesPage : Page
+    {
+        public SubcategoriesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
